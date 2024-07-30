e6a9b847b23d599869fd3388cc23a6f40314183a
Cobus <cobusreyneke1004@gmail.com>
2024-07-30 21:27:27 +0200

    [#9]

--- Greeter.java
+++ Greeter.java
@@ -1,5 +1,18 @@
-public class Greeter {
-	public static void main (String[] args) {
+public class Greeter { 
 
-	}
-}
+public static void main(String[] args) { 
+
+} 
+
+  
+
+public static String getName() { 
+
+Scanner sc = new Scanner(System.in); 
+		System.out.print(“Please provide your name: “); 
+		String name = sc.nextLine(); 
+		return name;		 
+
+} 
+
+} 
