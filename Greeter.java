d27845cddfd292790fbd15ab1384b503de62b6c3
Cobus <cobusreyneke1004@gmail.com>
2024-07-30 21:21:57 +0200

    [#4] removed class file

--- Greeter.java
+++ Greeter.java
@@ -1,5 +1,25 @@
-public class Greeter {
-	public static void main (String[] args) {
+import java.util.Scanner; 
 
-	}
+public class Greeter { 
+
+public static void main(String[] args) { 
+
+greeter(); 
+
+} 
+
+  
+
+public static void greeter() { 
+
+Scanner sc = new Scanner(System.in); 
+
+System.out.print("Please provide your name: "); 
+
+String name = sc.nextLine(); 
+
+System.out.println("Hello there, " + name + " How are you doing"); 
+
+} 
+
 }
